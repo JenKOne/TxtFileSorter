448ee81e020ae72a1033d36f7daaa684434ebae5
Evgeniy Kochubeev <evgen.kochubeev@gmail.com>
2026-01-17 16:11:37 +0300

    читаем из txt, сортируем по спискам, записываем файл

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,24 +1,29 @@
 package org.example;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) {
-        List<Float> floats;
-        List<Integer> integers;
-        List<String> strings;
-        TxtFileReader txtFileReader = new TxtFileReader();
-        StringFilter stringFilter = new StringFilter();
-        Path path = Paths.get("D:\\IdeaProjects\\заданиеШифт\\TxtFilterUtil\\src\\main\\resources\\In1.txt");
+    public static void main(String[] args) throws IOException {
+//        List<Float> floats;
+//        List<Integer> integers;
+//        List<String> strings;
+//        TxtFileReader txtFileReader = new TxtFileReader();
+//        StringFilter stringFilter = new StringFilter();
+//        Path path = Paths.get("D:\\IdeaProjects\\заданиеШифт\\TxtFilterUtil\\src\\main\\resources\\In1.txt");
 
 
-        integers = stringFilter.integers(txtFileReader.readTxt(path));
-        floats = stringFilter.floats(txtFileReader.readTxt(path));
-        strings = stringFilter.strings(txtFileReader.readTxt(path));
-        System.out.println(integers);
-        System.out.println(floats);
-        System.out.println(strings);
+//        integers = stringFilter.integers(txtFileReader.readTxt(path));
+//        floats = stringFilter.floats(txtFileReader.readTxt(path));
+//        strings = stringFilter.strings(txtFileReader.readTxt(path));
+//        System.out.println(integers);
+//        System.out.println(floats);
+//        System.out.println(strings);
+
+        TxtFileWriter txtFileWriter = new TxtFileWriter();
+        txtFileWriter.writeFiles();
+        System.out.println("что-то произошло, проверяй");
     }
 }
--- src/main/java/org/example/TxtFileWriter.java
+++ src/main/java/org/example/TxtFileWriter.java
@@ -1,11 +1,37 @@
 package org.example;
 
 import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class TxtFileWriter {
-    Path textFile = Paths.get("sample.txt");
-    BufferedWriter bufferedWriter = Files.newBufferedWriter(textFile);
+    List<Float> floats;
+    List<Integer> integers;
+    List<String> strings;
+    TxtFileReader txtFileReader = new TxtFileReader();
+    StringFilter stringFilter = new StringFilter();
+    Path path = Paths.get("D:\\IdeaProjects\\заданиеШифт\\TxtFilterUtil\\src\\main\\resources\\In1.txt");
+
+
+    public void writeFiles() {
+        Path textFile = Paths.get("ints.txt");
+        integers = stringFilter.integers(txtFileReader.readTxt(path));
+        System.out.println(integers);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(textFile)))) {
+            for (Integer line : integers) {
+                writer.write(String.valueOf(line));
+                writer.newLine(); // добавляем перевод строки
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public TxtFileWriter() throws IOException {
+    }
 }
